5a07e2ff0273d298b2078cd166415f28f8346318
nidhish35 <malavnidhish35@gmail.com>
2025-02-18 19:49:52 +0530

    Ternary and list update

--- ternaryandlist/src/App.jsx
+++ ternaryandlist/src/App.jsx
@@ -1,15 +1,44 @@
-import { useState } from 'react'
-
 import './App.css'
 
 function App() {
+// const age = 10;
+// const green = true;
 
+const users = [
+  {name: "Nidhish" ,age: 21},
+  {name: "Ritika" ,age: 22},
+  {name: "Atharva" ,age: 24},
 
+] 
   return (
     <>
+    {/* <div>
+      {age <=13 ? <h1>overage</h1> : <h1>Underage</h1>}
+      <h1 style={{color : green ? "green" : "red"}}>This is wow </h1>
+
+      {green && <button className='cursor-pointer bg-black text-white'>Greet</button>}
+      </div>  */}
+
       
+      <div>
+        {users.map((user,key) =>{
+          return(
+            <User key = {key} name={user.name} age={user.age}/>
+          );
+        })}
+      </div>
     </>
   )
+}
+
+const User = (props) => {
+  return(
+    <div>
+      
+    {props.name}, {props.age}
+            
+    </div>
+  );
 }
 
 export default App
